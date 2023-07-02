ef9b90d0ccb8bbdf8c1cc0dee7367adc35a15e59
pravastaipo <pravastaipo666@gmail.com>
2023-07-02 16:04:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [dataTodo, setDataTodo] = useState('');
+
+  const handleChange = (e) => {
+    setDataTodo(e.target.value);
+  };
+  const handleClick = () => {
+    setTodoList([...todoList, dataTodo]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar">
+        <div className="title">
+          <p>-App Project-</p>
+          <h2>Todo List</h2>
+        </div>
+      </nav>
+      <div className="container">
+        <h3>input your todo list!</h3>
+        <input onChange={handleChange} />
+        <button onClick={handleClick}>input</button>
+      </div>
+      <div className="container">
+        <h1>Todo List Result</h1>
+        <div className="wrapper">
+          {todoList.map((todo) => {
+            return (
+              <div className="todo">
+                <h2>{todo}</h2>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <footer>
+        <p>created by <span>@pravastaipo</span></p>
+        <p>made with love ❤</p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
